0f5a2cae127b7f5d8f071eebd30f4a861021f72c
Антон Покидов <toshkapok@yandex.ru>
2023-08-07 20:34:13 +0300

    1

---
+++ cats/pagination.py
@@ -0,0 +1,5 @@
+# cats/pagination.py
+from rest_framework.pagination import PageNumberPagination
+
+class CatsPagination(PageNumberPagination):
+    page_size = 20
--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'djoser',
     'cats.apps.CatsConfig',
 ]
@@ -135,6 +136,27 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        # Не будем подключать класс AnonRateThrottle глобально.
+        # Подключим его только в тех view-классах или вьюсетах,
+        # где надо установить лимиты для анонимов
+        'rest_framework.throttling.ScopedRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        # Но сами лимиты установим, и они будут доступны из всего кода проекта
+        'user': '10000/day', #  Лимит для UserRateThrottle
+        'anon': '1000/day',  #  Лимит для AnonRateThrottle
+        'low_request': '1/minute',
+    }
+} 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+} 
 
 SIMPLE_JWT = {
    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
